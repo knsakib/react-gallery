d0d32db548696e6209c0984ffafdec3ec20a9a86
knsakib <knsakib@gmail.com>
2019-12-22 11:01:27 -0500

    Updates

---
+++ precache-manifest.f7f747f6ef0d22b57d6c03c43d02a17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a5e1070ba32fe42fd2235a4370ee01",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "269b6043b215526cd4f6",
+    "url": "/react-gallery/static/css/main.d0a36e07.chunk.css"
+  },
+  {
+    "revision": "b29c865250f000286577",
+    "url": "/react-gallery/static/js/2.0c44cf1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gallery/static/js/2.0c44cf1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "269b6043b215526cd4f6",
+    "url": "/react-gallery/static/js/main.c56f369f.chunk.js"
+  },
+  {
+    "revision": "1afcba19630fc1e95e7b",
+    "url": "/react-gallery/static/js/runtime-main.210023d2.js"
+  }
+]);
